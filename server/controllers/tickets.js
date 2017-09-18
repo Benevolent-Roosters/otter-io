32c1af0a61693d1f4d3e5a4e04e95a68fa2ae341
Brendan Bansavage <brendaniscool@gmail.com>
2017-09-18 12:46:18 -0700

    accommodate post requests from cli

--- server/controllers/tickets.js
+++ server/controllers/tickets.js
@@ -23,7 +23,6 @@ module.exports.getPanelTickets = (req, res) => {
 };
 
 module.exports.createPanelTicket = (req, res) => {
-  console.log(req.body);
   if (helper.checkUndefined(
     req.body.title,
     req.body.description,
@@ -38,17 +37,23 @@ module.exports.createPanelTicket = (req, res) => {
     res.status(400).send('one of parameters from client is undefined');
     return;
   }
-  if (parseInt(req.body.creator_id) !== req.user.id) {
-    res.status(400).send('creator_id field from client doesnt match actual logged in user');
-    return;
+
+  // Match ticket creator to user for web client only
+  if (req.user) {
+    if (parseInt(req.body.creator_id) !== req.user.id) {
+      res.status(400).send('creator_id field from client doesnt match actual logged in user');
+      return;
+    }
+    req.body.creator_id = req.user.id;
   }
+
   var ticketObj = {
     title: req.body.title,
     description: req.body.description,
     status: req.body.status,
     priority: req.body.priority,
     type: req.body.type,
-    creator_id: req.user.id,
+    creator_id: req.body.creator_id,
     assignee_handle: req.body.assignee_handle,
     board_id: req.body.board_id,
     panel_id: req.body.panel_id
@@ -83,11 +88,11 @@ module.exports.createPanelTicket = (req, res) => {
 };
 
 module.exports.getOneTicket = (req, res) => {
-  if (helper.checkUndefined(req.params.id)) {
+  if (helper.checkUndefined(req.params.id) && helper.checkUndefined(req.query.id)) {
     res.status(400).send('one of parameters from client is undefined');
     return;
   }
-  var ticketId = req.params.id;
+  var ticketId = req.params.id || req.query.id;
   dbhelper.getTicketById(parseInt(ticketId))
     .then(ticket => {
       if (!ticket) {
@@ -106,6 +111,9 @@ module.exports.updateTicket = (req, res) => {
     return;
   }
   var ticketObj = req.body;
+
+  delete ticketObj.api_key;
+
   var validKeys = {
     'id': true,
     'title': true,
@@ -133,6 +141,7 @@ module.exports.updateTicket = (req, res) => {
   }
   dbhelper.getTicketById(parseInt(ticketId))
     .then(ticket => {
+      console.log('HEY', ticket);
       if (!ticket) {
         throw 'cant get ticket by id';
       }
