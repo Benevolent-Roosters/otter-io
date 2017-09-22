a454e31f384742b50023f566f7d3a153db956662
karunpav <k.pavithran@wustl.edu>
2017-09-21 18:35:41 -0700

    Update auth.js
    destroyed session port

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,6 @@
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
-const redisClient = require('redis').createClient('6379', process.env.REDIS_URL || 'localhost');
+const redisClient = require('redis').createClient(process.env.REDIS_URL || 'localhost');
 const models = require('../../db/models');
 const dbhelper = require('../../db/helpers.js');
 const helper = require('../controllers/helper');
