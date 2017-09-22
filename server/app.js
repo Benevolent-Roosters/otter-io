e093ec0f5f01f188bb39be99e67c5b10f0b69f24
karunpav <k.pavithran@wustl.edu>
2017-09-21 20:00:55 -0700

    Update app.js
    it didn't work

--- server/app.js
+++ server/app.js
@@ -25,4 +25,6 @@ app.use('/api', middleware.auth.verifyElse401, routes.api);
 app.use('/api/profiles', middleware.auth.verifyElse401, routes.profiles);
 app.use('/cli', middleware.auth.verifyAPIKey, routes.cli);
 
+app.use('*', (req, res) => res.render('index'));
+
 module.exports = app;
